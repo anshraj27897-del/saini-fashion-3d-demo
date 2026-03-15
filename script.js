422d434bd72ba8b47fe55803fc7a97b82dc2a975
Ansh Raj <anshraj27897@gmail.com>
2026-03-15 20:20:22 +0530

    Add product popup functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Saini Fashion Hub 3D demo loaded");
+function openProduct(name,price){
+
+document.getElementById("popup").style.display="flex"
+
+document.getElementById("productName").innerText=name
+document.getElementById("productPrice").innerText=price
+
+document.getElementById("whatsappLink").href=
+"https://wa.me/918107053133?text=I want to buy "+name
+
+}
+
+function closeProduct(){
+
+document.getElementById("popup").style.display="none"
+
+}
